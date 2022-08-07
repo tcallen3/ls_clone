9597ae531eaec5cb2fbcfba5501e66330c417047
Thomas Allen <allen.thomas.c@gmail.com>
2022-08-07 00:34:06 -0700

    get basic command line parsing in place

--- ls.c
+++ ls.c
@@ -1,12 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
+void
+usage(const char *synopsis)
+{
+	fprintf(stderr, "usage: %s [-%s] [file...]\n", getprogname(), 
+	    synopsis);
+	exit(EXIT_FAILURE);
+}
 
 int
 main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
+	int ch;
+	const char *all_opts = "oj";
+	const char *synopsis = "oj";
 
-	printf("Hello, world!\n");
+	setprogname(argv[0]);
+
+	while ((ch = getopt(argc, argv, all_opts)) != -1) {
+		switch (ch) {
+		case 'o':
+			printf("Option 'o' selected\n");
+			break;
+		case 'j':
+			printf("Option 'j' selected\n");
+			break;
+		case '?':
+		default:
+			usage(synopsis);
+			/* NOTREACHED */
+		}
+	}
+	
+	argc -= optind;
+	argv += optind;
+
+	while (argc > 0) {
+		printf("File argument: %s\n", argv[0]);
+		argc--;
+		argv++;
+	}
+
 	return EXIT_SUCCESS;
 }
