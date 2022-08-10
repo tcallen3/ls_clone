e17205cc1508e585213453aeda41d7c8b142ca9c
Thomas Allen <allen.thomas.c@gmail.com>
2022-08-09 18:59:52 -0700

    get most basic ls working for cwd

--- ls.c
+++ ls.c
@@ -1,6 +1,13 @@
+#include <dirent.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
+
+typedef struct options {
+	int show_dot_dirs;
+} options;
 
 void
 usage(const char *synopsis)
@@ -10,22 +17,39 @@ usage(const char *synopsis)
 	exit(EXIT_FAILURE);
 }
 
+void listDirectory(const char *dir_name, const options *ls_options)
+{
+	DIR *dp;
+	struct dirent *dir_entry;
+	if ((dp = opendir(dir_name)) == NULL) {
+		fprintf(stderr, "Could not open directory %s: %s\n",
+		    dir_name, strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	while ((dir_entry = readdir(dp)) != NULL) {
+		if (dir_entry->d_name[0] != '.' || 
+		    ls_options->show_dot_dirs) {
+			printf("%s\n", dir_entry->d_name);
+		}
+	}
+}
+
 int
 main(int argc, char *argv[])
 {
 	int ch;
-	const char *all_opts = "oj";
-	const char *synopsis = "oj";
+	const char *all_opts = "a";
+	const char *synopsis = "a";
+	const char *cwd_name = ".";
+	options prog_options;
 
 	setprogname(argv[0]);
 
 	while ((ch = getopt(argc, argv, all_opts)) != -1) {
 		switch (ch) {
-		case 'o':
-			printf("Option 'o' selected\n");
-			break;
-		case 'j':
-			printf("Option 'j' selected\n");
+		case 'a':
+			printf("Option 'a' selected\n");
+			prog_options.show_dot_dirs = 1;
 			break;
 		case '?':
 		default:
@@ -36,6 +60,11 @@ main(int argc, char *argv[])
 	
 	argc -= optind;
 	argv += optind;
+
+	if (argc == 0) {
+		/* if no files are specified, list contents of CWD */
+		listDirectory(cwd_name, &prog_options);
+	}
 
 	while (argc > 0) {
 		printf("File argument: %s\n", argv[0]);
