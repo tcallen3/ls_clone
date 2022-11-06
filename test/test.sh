b21192351e95e5a5f76a8b05618559d68411ffbd
Thomas Allen <allen.thomas.c@gmail.com>
2022-11-05 21:09:35 -0700

    flesh out testing script

--- test/test.sh
+++ test/test.sh
@@ -18,6 +18,9 @@ fi
 TSYS="/tmp/sys_ls"
 TMINE="/tmp/my_ls"
 
+# WARNING: these tests will fail if you run them on a directory with
+# block/character files (see note below on ls formatting), but this
+# should be solely due to whitespace changes
 OPTIONS="${DIR}
 -la ${DIR}
 -lai ${DIR}
@@ -29,27 +32,69 @@ OPTIONS="${DIR}
 -lF ${DIR}
 -A ${DIR}
 -w ${DIR}
--q ${DIR}"
+-q ${DIR}
+-lks ${DIR}
+/ /tmp ~ ."
 
 IFS="
 "
 
-for opt in ${OPTIONS}
-do
-	echo "testing options: ${opt}"
-	COM1="ls ${opt} | sed /total/d > ${TSYS}"
-	COM2="${MY_LS} ${opt} > ${TMINE}"
+save_cmd_output()
+{
+	COM1="ls ${1} | sed /total/d | sed /^$/d > ${TSYS}"
+	COM2="${MY_LS} ${1} > ${TMINE}"
 	sh -c "eval ${COM1}"
 	sh -c "eval ${COM2}"
+}
+
+for opt in ${OPTIONS}
+do
+	echo "Running test: ls ${opt}"
+	save_cmd_output ${opt}
+	# if you want to run all tests on block/char device files, change
+	# -b to -w in the diff
 	diff -bq ${TSYS} ${TMINE} || echo "ls ${opt} failed"
 done
 
-# system ls uses weird whitespace for char/block files
-WS_OPTIONS="-l /dev"
+# tests where other whitespace is involved (\t,\n,...), split out so
+# we can run previous tests on cases with weird nonpriting WS in filenames
+# and not ignore mismatches between ls and our code
+save_cmd_output "-l /dev"
+diff -bw ${TSYS} ${TMINE} || echo "ls ${opt} failed"
 
-BLOCK_OPTIONS=""
+env_cmd_output()
+{
+	COM1="${1} ls ${2} 2>/dev/null | sed /total/d | sed /^$/d > ${TSYS}"
+	COM2="${1} ${MY_LS} ${2} 2>/dev/null > ${TMINE}"
+	sh -c "eval ${COM1}"
+	sh -c "eval ${COM2}"
+}
 
-TZ_OPTIONS=""
+# these require setting BLOCKSIZE variable while they run
+BLOCK_OPTIONS="BADVAL
+0
+2048
+-25
+50"
+
+for opt in ${BLOCK_OPTIONS}
+do
+	echo "Running test with BLOCKSIZE=${opt}"
+	env_cmd_output "BLOCKSIZE=${opt}" "-ls ."
+	diff -qb ${TSYS} ${TMINE} || echo "BLOCKSIZE=${opt} failed"
+done
+
+# these require setting TZ variable while they run
+TZ_OPTIONS="EST5
+PST8PDT
+BADVAL"
+
+for opt in ${TZ_OPTIONS}
+do
+	echo "Running test with TZ=${opt}"
+	env_cmd_output "TZ=${opt}" "-ls ."
+	diff -qb ${TSYS} ${TMINE} || echo "BLOCKSIZE=${opt} failed"
+done
 
 # recursive timeout test?
 
